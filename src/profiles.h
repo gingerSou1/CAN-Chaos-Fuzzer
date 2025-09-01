52f58147fef4564c7d4c54da8ff8bb80c580cf88
gingerSou1 <114539015+gingerSou1@users.noreply.github.com>
2025-08-31 20:08:49 -0500

    Add files via upload

--- src/profiles.h
+++ src/profiles.h
@@ -1,12 +1,1 @@
-# pragma once
-struct FuzzProfile {
-  const char* name;
-  uint16_t baseIdMin;
-  uint16_t baseIdMax;
-  uint8_t  lenMin;
-  uint8_t  lenMax;
-  uint8_t  bitflipPct;    // 0..100 chance to flip a random bit
-  uint16_t jitterMs;      // +/- jitter applied to tx interval
-  uint16_t burstCount;    // number of frames per burst (0 = disabled)
-  uint16_t burstCooldownMs;
-};
+#pragma once
